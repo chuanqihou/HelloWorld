18d3010101a3f21803d2518e535d9c854a0414d7
传奇后 <1762474659@qq.com>
2021-09-12 10:35:41 +0800

    HelloWorld!

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @auther 传奇后
+ * @date 2021/9/12 10:30
+ * @veersion 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
