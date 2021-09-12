3d71fafba2080570cbfd74ce82506f76cfab42d3
传奇后 <1762474659@qq.com>
2021-09-12 10:44:46 +0800

    version01

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
         System.out.println("Hello");
+        System.out.println("version01");
     }
 }
